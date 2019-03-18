2d052ced3806e5d1bb5f743e1ccb7e2825baaab6
ingoo <web7722@gmail.com>
2019-03-18 13:12:11 +0900

    Create for.py

---
+++ for.py
@@ -0,0 +1,7 @@
+score = [100, 55, 70 ,35 , 90]
+for number in range(len(score)):
+    if(score[number]) >= 60):
+        print("%d 학생 합격" % (number+1)) 
+    else:
+        print("%d 학생 불합격" % (number+1))
+        
