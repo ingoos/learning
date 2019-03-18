bdb26fb6bf503d81b895e9d81ef3d4b01d066492
ingoo <web7722@gmail.com>
2019-03-18 16:47:50 +0900

    update crawler.py

---
+++ crawler.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+def spider(max_pages):
+    print('sample')
+    page = 1
+    while page < max_pages:
+        print('inner')
+        url = 'http://naver.com'
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'lxml')
+        page += 1
+    print(soup)
+    
+    #print(soup)
+
+spider(2)
