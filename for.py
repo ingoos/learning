11e6e2fe04d9b7451452666d6dda11cb98ad462b
ingoo <web7722@gmail.com>
2019-03-18 13:17:14 +0900

    4line update for.py

--- for.py
+++ for.py
@@ -1,7 +1,6 @@
 score = [100, 55, 70 ,35 , 90]
 for number in range(len(score)):
-    if(score[number]) >= 60):
+    if((score[number]) >= 60):
         print("%d 학생 합격" % (number+1)) 
     else:
         print("%d 학생 불합격" % (number+1))
-        
